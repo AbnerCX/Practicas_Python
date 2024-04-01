d3e929b2871415fa3941d210e7521a75e705d7f5
AbnerCX <abner234.lg@gmail.com>
2024-04-01 17:26:49 -0600

    New content for Ejercicio_17.py

--- Ejercicios/ejercicio_17.py
+++ Ejercicios/ejercicio_17.py
@@ -1,1 +1,40 @@
-print('Bueno mejor modificare esto jaja')
+class Console:
+    def __init__(self, marca, color, almacenamiento, precio):
+        self.marca = marca
+        self.color = color
+        self.almacenamiento = almacenamiento
+        self.precio = precio
+
+    def show_information(self):
+        print(f'''INFORMACION
+        Marca: {self.marca}
+        Color: {self.color}
+        Almacenamiento: {self.almacenamiento}
+        Precio: {self.precio}
+            ''')
+
+class PlayStation(Console):
+    def __init__(self, marca, color, almacenamiento, precio, mando):
+        super().__init__(marca, color, almacenamiento, precio)
+        self.mando = mando
+
+    def juegos_exclusivos(self):
+        print('PlayStation tiene juegos exclusivos.')
+
+class Xbox(PlayStation):
+    def __init__(self, marca, color, almacenamiento, precio, mando, baterias):
+        super().__init__(marca, color, almacenamiento, precio, mando)
+        self.baterias = baterias
+
+    def las_baterias(self):
+        print('Xbox necesita baterias en 2024.')
+
+
+consola = Console('Pc', 'Negra', '256', '500 USD')
+consola.show_information()
+
+playstation = PlayStation('Pley', 'Blanco', '500', '600 USD', 'DualShock')
+playstation.juegos_exclusivos()
+
+xbox = Xbox('ecsbox', 'Negro', '500', '450', 'Xbox Controller', 'AAA')
+xbox.las_baterias()
